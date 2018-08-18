85b27176012cddec33a76a6c3c90024ec02f16f9
dkbrookie <matthew@dkbinnovative.net>
2018-08-18 03:58:25 -0500

    Added $sysDrive var

--- CCleaner Auto Run.ps1
+++ CCleaner Auto Run.ps1
@@ -3,8 +3,10 @@ powershell.exe -command "& {(new-object Net.WebClient).DownloadString('https://g
 #>
 
 ##Set dir vars
+$OS = Get-WMiobject -Class Win32_operatingsystem
+$sysDrive = $OS.SystemDrive
 $ccleaner = "https://automate.manawa.net/labtech/transfer/software/ccleaner/ccleaner.exe"
-$ltPath = "C:\Windows\LTSvc"
+$ltPath = "$sysDrive\Windows\LTSvc"
 $packagePath = "$ltPath\Packages"
 $softwarePath = "$packagePath\Software"
 $ccleanerPath = "$softwarePath\CCleaner"
@@ -13,7 +15,7 @@ $ccleanerLaunch = "$ccleanerPath\CCleaner.exe"
 
 Try{
     ##Finds C disk space before ccleaner runs
-    $diskBefore = Get-WmiObject Win32_LogicalDisk | Where {$_.DeviceID -eq "C:"}
+    $diskBefore = Get-WmiObject Win32_LogicalDisk | Where {$_.DeviceID -eq $sysDrive}
 }
 Catch{
     Write-Warning "Failed to get disk space $_.Exception.Message"
@@ -63,7 +65,7 @@ Write-Output "Cleaning Complete"
 
 
 ##Gets the free space of C drive intended to use after the clean function to calculate total disk space saved
-$diskAfter = Get-WmiObject Win32_LogicalDisk | Where {$_.DeviceID -eq "C:"}
+$diskAfter = Get-WmiObject Win32_LogicalDisk | Where {$_.DeviceID -eq $sysDrive}
 
 
 ##Uses the values from CC-getDiskStart and CC-getDiskEnd to calculate total space saved, then converts it to MBs for easier reading
