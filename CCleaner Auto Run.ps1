0e49e9761421df420360dfda5edc6902efb2b730
dkbrookie <matthew@dkbinnovative.net>
2018-08-19 02:22:06 -0500

    Added additional removals
    -Added SoftwareDistribution/Downloads removal
    -Added Windows.old removal
    -Added Windows10Upgrade removal

--- CCleaner Auto Run.ps1
+++ CCleaner Auto Run.ps1
@@ -2,6 +2,18 @@
 powershell.exe -command "& {(new-object Net.WebClient).DownloadString('https://goo.gl/RUhMFJ') | iex}
 #>
 
+Function Get-Tree($Path,$Include='*'){
+    @(Get-Item $Path -Include $Include -Force) +
+        (Get-ChildItem $Path -Recurse -Include $Include -Force) | Sort PSPath -Descending -Unique
+}
+
+Function Remove-Tree($Path,$Include='*'){
+    Get-Tree $Path $Include | Remove-Item -Force -Recurse
+}
+
+
+
+#region
 Write-Output "===CCleaner Auto Clean==="
 ##Set dir vars
 $OS = Get-WMiobject -Class Win32_operatingsystem
@@ -49,12 +61,50 @@ If(!$downloadStatus){
 Else{
     Write-Output "Verified CCleaner.exe exists at $ccleanerPath"
 }
+#endregion
 
 ##Starts the CCleaner process
 Write-Output "Starting Cleaning Process"
 Start-Process -FilePath $ccleanerLaunch -ArgumentList "/AUTO"
 Wait-Process -Name CCleaner
 Write-Output "Cleaning Complete"
+
+##CLear out Windows.old
+If(Test-Path "$sysDrive\Windows.old"){
+    cmd.exe /c "icacls $sysDrive\Windows.old /grant Everyone:(OI)(CI)F /q"
+    cmd.exe /c "takeown /f $sysDrive\Windows.old"
+    cmd.exe /c "rd $sysDrive\Windows.old /q /s"
+    If(Test-Path "$sysDrive\Windows.old"){
+        Write-Output "Removed $sysDrive\Windows.old"
+    }
+    ELSE{
+        Write-Output "Failed to remove $sysDrive\Windows.old"
+    }
+}
+
+##Clear out the Windows 10 Upgrade folder
+If(Test-Path "$sysDrive\Windows10Upgrade"){
+    cmd.exe /c "icacls $sysDrive\Windows10Upgrade /grant Everyone:(OI)(CI)F /q"
+    cmd.exe /c "takeown /f $sysDrive\Windows10Upgrade"
+    Remove-Tree $sysDrive\Windows10Upgrade
+    If(Test-Path "$sysDrive\Windows10Upgrade"){
+        Write-Output "Removed $sysDrive\Windows10Upgrade"
+    }
+    ELSE{
+        Write-Output "Failed to remove $sysDrive\Windows10Upgrade"
+    }
+}
+
+##Clear SoftwareDistribution Downloads
+If(Test-Path "$sysDrive\Windows\SoftwareDistribution\Downloads"){
+    Remove-Tree $sysDrive\Windows\SoftwareDistribution\Downloads
+    If(Test-Path "$sysDrive\Windows\SoftwareDistribution\Downloads"){
+        Write-Output "Removed $sysDrive\Windows\SoftwareDistribution\Downloads"
+    }
+    ELSE{
+        Write-Output "Failed to remove $sysDrive\Windows10Upgrade"
+    }
+}
 
 ##Gets the free space of C drive intended to use after the clean function to calculate total disk space saved
 $diskAfter = Get-WmiObject Win32_LogicalDisk | Where {$_.DeviceID -eq $sysDrive}
