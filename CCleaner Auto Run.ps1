fc0d1ec9b0d8a17f3d8d9ed702bf3d94dd1644ce
dkbrookie <matthew@dkbinnovative.net>
2018-09-04 00:05:41 -0500

    Excluded cookie removal

--- CCleaner Auto Run.ps1
+++ CCleaner Auto Run.ps1
@@ -20,6 +20,7 @@ Function CC-fileCheck{
     ##Set dir vars
     $OS = Get-WMiobject -Class Win32_operatingsystem
     $ccleaner = "https://automate.manawa.net/labtech/transfer/software/ccleaner/ccleaner.exe"
+    $ccleanerConfig = "https://automate.manawa.net/labtech/transfer/software/ccleaner/ccleaner.ini"
     $ltPath = "$sysDrive\Windows\LTSvc"
     $packagePath = "$ltPath\Packages"
     $softwarePath = "$packagePath\Software"
@@ -54,9 +55,22 @@ Function CC-fileCheck{
             Break
         }
     }
-
     Else{
         Write-Output "Verified CCleaner.exe exists at $ccleanerPath"
+    }
+
+    $downloadStatusConfig = Test-Path "$ccleanerPath\ccleaner.ini" -PathType Leaf
+    If(!$downloadStatusConfig){
+        IWR -Uri $ccleanerConfig -Outfile $ccleanerPath\ccleaner.ini | Out-Null
+        Write-Output "No CCleaner.ini found in $ccleanerpath, downloaded CCleaner.ini"
+        $downloadStatus = Test-Path "$ccleanerPath\ccleaner.ini" -PathType Leaf
+        If(!$downloadStatus){
+            Write-Output "!ERRDL01: Failed to download CCleaner.ini from $ccleanerConfig, exiting script"
+            Break
+        }
+    }
+    Else{
+        Write-Output "Verified CCleaner.ini exists at $ccleanerPath"
     }
 }
 
@@ -112,7 +126,7 @@ Function DC-diskClean{
     Else{
         Write-Output "$sysDrive\Windows\System32\)cleamngr.exe is not present on this machine"
     }
-}ql
+}
 
 CC-fileCheck
 CC-startClean
