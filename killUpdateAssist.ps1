1b5ab4b8a6ea8468ca0c425a61c61deaac76bc02
dkbrookie <matthew@dkbinnovative.net>
2018-10-05 02:08:34 -0500

    fixed cacls cmd

--- killUpdateAssist.ps1
+++ killUpdateAssist.ps1
@@ -14,8 +14,8 @@ Stop-Process -Name Windows10Upgrade -Force
 
 If(Test-Path $path1){
     Write-Output "!DEL: Deleted $path1"
-    cmd.exe /c "takeown /F $path1\* /R /A"
-    cmd.exe /c "cacls $path1\*.* /T /grant administrators:F"
+    cmd.exe /c "echo y| takeown /F $path1\* /R /A"
+    cmd.exe /c "echo y| cacls $path1\*.* /T /grant administrators:F"
     Remove-Tree $path1
 }
 Else{
@@ -24,8 +24,8 @@ Else{
 
 If(Test-Path $path2){
     Write-Output "!DEL: Deleted $path2"
-    cmd.exe /c "takeown /F $path2\* /R /A"
-    cmd.exe /c "cacls $path2\*.* /T /grant administrators:F"
+    cmd.exe /c "echo y| takeown /F $path2\* /R /A"
+    cmd.exe /c "echo y| cacls $path2\*.* /T /grant administrators:F"
     Remove-Tree $path2
 }
 Else{
@@ -42,3 +42,7 @@ If($tasks){
 Else{
     Write-Output "No scheduled tasks found for UpdateAssistant"
 }
+
+
+
+cmd.exe /c "cacls C:\Windows\UpdateAssistant\*.* /T /grant administrators:F"
