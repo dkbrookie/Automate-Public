ff98446580f950ece0e69896520473ff936c11c8
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2019-10-30 00:16:22 -0500

    Update Disk_Cleanup.ps1
    -Added much improved disk cleanup features

--- Maintenance/Disk Cleanup/Powershell/Disk_Cleanup.ps1
+++ Maintenance/Disk Cleanup/Powershell/Disk_Cleanup.ps1
@@ -18,41 +18,41 @@ $ccleanerExe = "$ccleanerDir\CCleaner.exe"
 $ccleanerIni = "$ccleanerDir\ccleaner.ini"
 $logFile = "$sysDrive\Windows\LTSvc\diskCleanup.txt"
 
-If(!(Test-Path $ccleanerDir)) {
-  New-Item -Path $ccleanerDir -ItemType Directory | Out-Null
+If (!(Test-Path $ccleanerDir)) {
+    New-Item -Path $ccleanerDir -ItemType Directory | Out-Null
 }
 
 Try {
-  If ((Test-Path $ccleanerExe -PathType Leaf)) {
-    Write-Output 'Ccleaner.exe exists, checking file size...'
-    ## For some reason when downloading from google drive once and awhile it just won't finish the download
-    ## so a byte check really is necessary to make sure CCleaner downloads succesfully. Long play is to move
-    ## this to an FTP server, just haven't had a chance.
-    If ((Get-Item $ccleanerExe -EA 0).Length -ne '13594584') {
-      Write-Warning 'Ccleaner does exist, but the file size does not match the server. Re-downloading...'
-      (New-Object System.Net.WebClient).DownloadFile($ccleanerUrl, $ccleanerExe)
+    If ((Test-Path $ccleanerExe -PathType Leaf)) {
+        Write-Output 'Ccleaner.exe exists, checking file size...'
+        ## For some reason when downloading from google drive once and awhile it just won't finish the download
+        ## so a byte check really is necessary to make sure CCleaner downloads succesfully. Long play is to move
+        ## this to an FTP server, just haven't had a chance.
+        If ((Get-Item $ccleanerExe -EA 0).Length -ne '13594584') {
+        Write-Warning 'Ccleaner does exist, but the file size does not match the server. Re-downloading...'
+        (New-Object System.Net.WebClient).DownloadFile($ccleanerUrl, $ccleanerExe)
+        } Else {
+        Write-Output 'Verified Ccleaner.exe file size is correct, executing Ccleaner...'
+        }
     } Else {
-      Write-Output 'Verified Ccleaner.exe file size is correct, executing Ccleaner...'
+        Write-Warning 'Ccleaner.exe does not exist, downloading file...'
+        (New-Object System.Net.WebClient).DownloadFile($ccleanerUrl, $ccleanerExe)
     }
-  } Else {
-    Write-Warning 'Ccleaner.exe does not exist, downloading file...'
-    (New-Object System.Net.WebClient).DownloadFile($ccleanerUrl, $ccleanerExe)
-  }
 
-  If(!(Test-Path $ccleanerIni -PathType Leaf)) {
-    (New-Object System.Net.WebClient).DownloadFile($ccleanerConfigUrl, $ccleanerIni)
-  }
+    If(!(Test-Path $ccleanerIni -PathType Leaf)) {
+        (New-Object System.Net.WebClient).DownloadFile($ccleanerConfigUrl, $ccleanerIni)
+    }
 } Catch {
-  Write-Error "!ERROR: Failed to download required files, exiting script"
+    Write-Error "!ERROR: Failed to download required files, exiting script"
 }
 
 
 ## Starts the CCleaner process
 Try {
-  Start-Process -FilePath $ccleanerExe -ArgumentList "/AUTO" -Wait
-  Write-Output 'CCleaner complete!'
+    Start-Process -FilePath $ccleanerExe -ArgumentList "/AUTO" -Wait
+    Write-Output 'CCleaner complete!'
 } Catch {
-  Write-Warning '!ERROR: There was a problem running Ccleaner.exe, unable to complete this task'
+    Write-Warning '!ERROR: There was a problem running Ccleaner.exe, unable to complete this task'
 }
 
 ## Deletes old windows update files and old versions of Windows
@@ -60,40 +60,97 @@ Try {
 ## permission issues instead of skipping and moving on. This is even with trying -Confirm:$False -Force etc.
 $folders = "$sysDrive\Windows10Upgrade","$sysDrive\Windows\SoftwareDistribution\Download","$sysDrive\Windows.old"
 ForEach($folder in $folders){
-  If((Test-Path $folder)){
-    &cmd.exe /c echo y| takeown /F $folder\* /R /A /D Y 2>&1 | Out-Null
-    &cmd.exe /c echo y| cacls $folder\*.* /T /grant administrators:F 2>&1 | Out-Null
-    &cmd.exe /c RD /S /Q $folder 2>&1 | Out-Null
-    Write-Output "Deleted $folder"
-  }
+    If((Test-Path $folder)){
+        &cmd.exe /c echo y| takeown /F $folder\* /R /A /D Y 2>&1 | Out-Null
+        &cmd.exe /c echo y| cacls $folder\*.* /T /grant administrators:F 2>&1 | Out-Null
+        &cmd.exe /c RD /S /Q $folder 2>&1 | Out-Null
+        Write-Output "Deleted $folder"
+    }
 }
+Write-Output "Windows temp system file cleanup complete!"
+
 
 ## Deletes temp files
 ## Using CMD RD instead of Remove-Item for same reasons as above
-$folders = "$env:TEMP","$env:SystemDrive\Temp","$env:windir\Temp"
-ForEach($folder in $folders){
-  If((Test-Path $folder)){
-    &cmd.exe /c RD /S /Q $folder 2>&1 | Out-Null
-    Write-Output "Deleted temp files from $folder"
-  }
+$tempCount = 0
+If (!$excludeCTemp) {
+    $folders = "$env:TEMP","$env:SystemDrive\Temp","$env:windir\Temp"
+} Else {
+     $folders = "$env:TEMP","$env:windir\Temp"
+}
+ForEach ($folder in $folders) {
+    Get-ChildItem $folder -Recurse -EA 0 | ForEach-Object {
+        $tempCount++
+        $item = $_.FullName
+        Try {
+        Remove-Item $item -Recurse -Force -ErrorAction Stop
+        #Write-Output "Deleted $item"
+        } Catch {
+        $tempCount--
+        #Write-Warning "Failed to delete $item"
+        }
+    }
 }
 
-## Verifies disk cleanup is present, runs it if true
-If((Test-Path "$env:windir\System32\cleanmgr.exe" -PathType Leaf)) {
-  $diskCleanRegPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\'
-  ## Every item in this long list below is an item on DiskCleanup that will be checked and cleaned through this script
-  $cleanItems = 'Active Setup Temp Folders','BranchCache','Content Indexer Cleaner','D3D Shader Cache','Delivery Optimization Files','Device Driver Packages','Diagnostic Data Viewer database files','Downloaded Program Files','DownloadsFolder','Internet Cache Files','Language Pack','Offline Pages Files','Old ChkDsk Files','Previous Installations','Recycle Bin','RetailDemo Offline Content','Service Pack Cleanup','Setup Log Files','System error memory dump files','System error minidump files','System error minidump files','Temporary Setup Files','Thumbnail Cache','Update Cleanup','User file versions','Windows Defender','Windows Error Reporting Files','Windows ESD installation files','Windows Upgrade Log Files'
+If ($tempCount -eq 0) {
+    $folders = $folders.Split(' ')
+    Write-Output "No temp items were found that can be removed at this time from $folders"
+} Else {
+    Write-Output "Successfully removed $tempCount temp items, general temp file removal complete!"
+}
 
-  ForEach ($item in $cleanItems) {
-    $curProperty = Get-ItemProperty -Path "$diskCleanRegPath\$item" -Name StateFlags0777 -EA 0
-    If (!$curProperty -or $curProperty.StateFlags0777 -ne 2) {
-      Write-Output "Setting $item to enabled in Disk Cleanup"
-      New-ItemProperty -Path "$diskCleanRegPath\$item" -Name StateFlags0777 -Value 2 -PropertyType DWORD -EA 0 | Out-Null
+
+## Verifies disk cleanup is present, runs it if true
+If ((Test-Path "$env:windir\System32\cleanmgr.exe" -PathType Leaf)) {
+    $diskCleanRegPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\'
+    ## Every item in this long list below is an item on DiskCleanup that will be checked and cleaned through this script
+    $cleanItems = 'Active Setup Temp Folders','BranchCache','Content Indexer Cleaner','D3D Shader Cache','Delivery Optimization Files','Device Driver Packages','Diagnostic Data Viewer database files','Downloaded Program Files','DownloadsFolder','Internet Cache Files','Language Pack','Offline Pages Files','Old ChkDsk Files','Previous Installations','Recycle Bin','RetailDemo Offline Content','Service Pack Cleanup','Setup Log Files','System error memory dump files','System error minidump files','System error minidump files','Temporary Setup Files','Thumbnail Cache','Update Cleanup','User file versions','Windows Defender','Windows Error Reporting Files','Windows ESD installation files','Windows Upgrade Log Files'
+
+    ForEach ($item in $cleanItems) {
+        $curProperty = Get-ItemProperty -Path "$diskCleanRegPath\$item" -Name StateFlags0777 -EA 0
+        If (!$curProperty -or $curProperty.StateFlags0777 -ne 2) {
+        Write-Output "Setting $item to enabled in Disk Cleanup"
+        New-ItemProperty -Path "$diskCleanRegPath\$item" -Name StateFlags0777 -Value 2 -PropertyType DWORD -EA 0 | Out-Null
+        }
     }
-  }
-  &cmd.exe /c echo y| cleanmgr /sagerun:0777
-  Wait-Process cleanmgr
-  #Start-Process cleanmgr -ArgumentList "/SAGERUN:0777" -Wait -NoNewWindow
+    &cmd.exe /c echo y| cleanmgr /sagerun:0777
+    ## Check to see if cleanmgr is still running since this often gets hung up and stops the script. This is
+    ## pretty extensive just to check if cleanmgr is running, but the alternative is letting this get hung up
+    ## and the script in Automate just gets stuck untilt he couple hour timeout hits. 
+    $proc = 'cleanmgr'
+    If (Get-Process $proc -EA 0) {
+        ## Define the number of minutes the countdown timer should allow it to gracefully finish before giving up and killing cleanmgr.
+        $CountDownTimer = 10
+        Write-Host " "
+        Write-Warning "$proc is still running. Waiting up to $CountDownTimer minutes for it to gracefully complete so the script can proceed."
+        $diskCleanupRunning = $True
+        $StartTime = Get-Date
+        $EndTime = $StartTime.AddMinutes($CountDownTimer)
+        $TimeSpan = New-TimeSpan (Get-Date) $EndTime
+    }
+    ## While the process is still running, or there is time remaining in the timespan countdown, loop through writing a progress bar and check to see if the process is still running.
+    While ($diskCleanupRunning -and ($TimeSpan.TotalSeconds -gt 0)) {
+        Write-Progress -Activity "Waiting for $proc to complete." -Status "$CountDownTimer minute countdown before the script kills $proc." `
+            -SecondsRemaining $TimeSpan.TotalSeconds
+        Start-Sleep -Seconds 1
+        ## Recheck to see if the process is still running. It will return a $Null/$False value if it isn't for evaluation at the beginning of the loop.
+        $diskCleanupRunning = Get-Process $proc -EA 0
+        ## Recalculate the new timespan at the end of the loop for evaluation at the beginning of the loop.
+        $TimeSpan = New-TimeSpan (Get-Date) $EndTime
+    }
+    ## Close out the progress bar cleanly if it was still running. 
+    Write-Progress -Activity "Waiting for $proc to complete." -Completed
+    ## If cleanmgr is still running after the end count time timer, kill cleanmgr. Otherwise note the time it took cleanmgr to complete.
+    If ($diskCleanupRunning) {
+        Write-Host "$proc is still running after the $CountDownTimer time expired, and will hang up the rest of this disk cleanup script."
+        Stop-Process -Name cleanmgr -Force
+    } ElseIf ($CountDownTimer) {
+        ## Calculate the completed time by taking the timespan of the start time versus the end time, where the end time has the seconds the loop took to run subtracted from it.
+        $CompletedTime = (New-TimeSpan $StartTime $Endtime.AddSeconds(-$TimeSpan.TotalSeconds)).ToString("mm':'ss")
+        Write-Host "$proc has finished after $CompletedTime."
+        Write-Host " "
+    }
+    Write-Host "$proc (built in windows disk cleanup) complete!"
 }
 
 
@@ -128,15 +185,15 @@ If ($dismWinSxs -like '*The operation could not be completed due to pending oper
 
 ## Empty recycle bin
 Try {
-  $definition = @'
+    $definition = @'
 [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
 public static extern uint SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, uint dwFlags);
 '@
-  $winApi = Add-Type -MemberDefinition $definition -Name WinAPI -Namespace Extern -PassThru
-  $winApi::SHEmptyRecycleBin(0, $null, 7)
-  Write-Output 'Recycling bin successfully emptied'
+    $winApi = Add-Type -MemberDefinition $definition -Name WinAPI -Namespace Extern -PassThru
+    $winApi::SHEmptyRecycleBin(0, $null, 7) | Out-Null
+    Write-Output 'Recycling bin successfully emptied'
 } Catch {
-  Write-Warning '!ERROR: There was a problem when attempting to empty the recycling bin, unable to complete this task.'
+    Write-Warning '!ERROR: There was a problem when attempting to empty the recycling bin, unable to complete this task.'
 }
 
 
@@ -147,12 +204,12 @@ $diskAfter = (Get-WmiObject Win32_LogicalDisk).FreeSpace | Measure-Object -Sum
 ## total space saved, then converts it to MBs for easier reading
 $before = [math]::Round($diskBefore.Sum/1MB,2)
 $after = [math]::Round($diskAfter.Sum/1MB,2)
-$saved = [math]::Round(($before - $after)/1MB,2)
+$saved = [math]::Round(($after - $before),2)
+$savedGBs = [math]::Round(($saved * 1024 * 1024 / 1GB),2)
 ## If there is less space than before the script started just report back 0
 If($saved -le 0){
-  $saved = 0
+    $saved = 0
 }
 
 ## Formats the output so we can split vars in Automate
-Write-Output "before=$before|after=$after|spaceSaved=$saved"
-Write-Output '***NOTE: before/after/saved is output in MBs'
+Write-Output "before=$before|after=$after|spaceSaved=$saved|spaceSavedGBs=$($savedGBs)GBs"
