9991ec8e59dc0a65ae4db4c5ac99025cabba671c
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2019-11-21 21:54:19 -0600

    Create Monitors_Hyper-V_Replication.ps1

---
+++ Monitors/Monitors_Hyper-V_Replication.ps1
@@ -0,0 +1,19 @@
+## Possible outputs from MS: NotApplicable, Normal, Warning, Critical
+
+Try {
+    $health = (Get-VMReplication).Health
+    $machine = $env:COMPUTERNAME
+    If ($health -eq 'Normal') {
+        Write-Output "!SUCCESS: Replication status is Normal on $machine"
+    } ElseIf ($health -eq 'Warning') {
+        Write-Output "!WARNING: Replication is in a warning state on $machine! Please address the issue immediately!"
+    } ElseIf ($health -eq 'Critical') {
+        Write-Output "!CRITICAL: Repliation status is critical on $machine! Please address the issue immidiately!"
+    } ElseIf ($health -eq 'NotApplicable') {
+        Write-Output "!SUCCESS: Replication check not applicable to $machine"
+    } Else {
+        Write-Output "Unknown output"
+    }
+} Catch {
+    Write-Output "$_"
+}
