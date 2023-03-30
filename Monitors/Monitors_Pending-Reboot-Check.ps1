7a2290cc9bde91c3ba7a41098d241c1033677fc1
Seth Brasile <seth.brasile@gmail.com>
2023-03-30 11:47:06 -0500

    Create Monitors_Pending-Reboot-Check.ps1

---
+++ Monitors/Monitors_Pending-Reboot-Check.ps1
@@ -0,0 +1,27 @@
+Try {
+  # Oddly, this command works to enable TLS12 on even Powershellv2 when it shows as unavailable. This also still works for Win8+
+  [Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([Net.SecurityProtocolType], 3072)
+} Catch {
+  Write-Output "Encountered an error while attempting to enable TLS1.2 to ensure successful file downloads. This can sometimes be due to dated Powershell. Checking Powershell version..."
+  # Generally enabling TLS1.2 fails due to dated Powershell so we're doing a check here to help troubleshoot failures later
+  $psVers = $PSVersionTable.PSVersion
+
+  If ($psVers.Major -lt 3) {
+    Write-Output "Powershell version installed is only $psVers which has known issues with this script directly related to successful file downloads. Script will continue, but may be unsuccessful."
+  }
+}
+
+# Call in Invoke-Output
+(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/dkbrookie/PowershellFunctions/master/Function.Invoke-Output.ps1') | Invoke-Expression
+# Call in Read-PendingRebootStatus
+(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/dkbrookie/PowershellFunctions/master/Function.Read-PendingRebootStatus.ps1') | Invoke-Expression
+
+Try {
+  $result = Read-PendingRebootStatus
+} Catch {
+  $result = @{
+    Output = "Encountered an error while attempting to read pending reboot status. Error: $($_.Exception.Message)"
+  }
+}
+
+Invoke-Output $result
